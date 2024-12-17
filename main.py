d06b9fc4bdb6b76a130d0a461991c7234449c163
Simon <simon.riou@phelma.grenoble-inp.fr>
2024-12-17 11:48:49 +0100

    Added verification for already cleaned files

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from utils import clean_pgn, export_features_labels, split_pgn
 from data_process import get_raw_fen, process_game, process_file
 
+import os
+
 file_path = 'datasets/blitz_2000_23.pgn'
 file_name = file_path.split('/')[-1].split('.')[0]
 stockfish_path = 'stockfish/stockfish-macos-m1-apple-silicon'
 
-game_fens = get_raw_fen(clean_pgn(file_path))
+game_fens = []
+# Check if the file has alreadys been cleaned
+if not os.path.exists(f"cleaned/{file_name}_cleaned.pgn"):
+    game_fens = get_raw_fen(clean_pgn(file_path))
+else:
+    print("Cleaned file was found. Skipping cleaning step.")
+    game_fens = get_raw_fen(f"cleaned/{file_name}_cleaned.pgn")
+
 
 # Splitting the PGN file
 for file_path in split_pgn(f"cleaned/{file_name}_cleaned.pgn", 100):
